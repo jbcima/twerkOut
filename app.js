552b6374fcce523d1ed7efd74fd6b2b388cc4ca1
jbcima <me@jeremycimafonte.com>
2013-08-03 17:52:36 -0400

    chief keef aint no hitta

--- app.js
+++ app.js
@@ -4,11 +4,21 @@
  */
 
 var express = require('express')
+  , app = express()
+  , server = require('http').createServer(app)
+  , io = require('socket.io').listen(server)
+  , port = 8080
   , routes = require('./routes')
   , http = require('http')
   , path = require('path');
 
-var app = express();
+
+if (process.env.NODE_ENV == 'production')
+  port = 80;
+
+server.listen(port);
+console.log('app running at localhost:' + port);
+
 
 app.configure(function(){
   app.set('port', process.env.PORT || 3000);
@@ -30,7 +40,34 @@ app.get('/', routes.index);
 app.get('/twerk', routes.twerk);
 
 
+io.sockets.on('connection', function(socket){
+  console.log('connection!');
+  socket.on('join', function(sessionID){
+    socket.set('sessionID', sessionID, function(){
+      if(socket.join(sessionID))
+        socket.broadcast.to(sessionID).emit('joined', sessionID);
+    });
+  });
+  socket.on('device-motion', function(data){
+    socket.get('sessionID', function(err, sessionID){
+      if (err) {
+        console.log(err);
+      } else if (sessionID) {
+        console.log(data);
+        socket.broadcast.to(sessionID).emit('action', data);
+      } else {
+        console.log("No sessionID");
+      }
+    });
+  });
+  socket.on('testing', function(data){
+    console.log(data);
+  });
+});
+
+
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log("Express server listening on port " + app.get('port'));
 });
+
