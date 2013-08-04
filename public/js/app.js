e6e8eb11256838fc25b9c98f573f61b3c774462c
jbcima <me@jeremycimafonte.com>
2013-08-04 03:10:24 -0400

    added app.js angular

---
+++ public/js/app.js
@@ -0,0 +1,11 @@
+// if the device is booty enabled. send them to twerk.
+if (screen.width <= 699) {
+  document.location = "twerk";
+}
+
+
+//'use strict';
+// :-(
+
+// Declare app level module which depends on filters, and services
+var app = angular.module('app', []);
