07509fd30d5e13ee8bfb9b93ee8ac43b4118e4ec
ricky <ricky.medina91@gmail.com>
2014-01-02 21:40:23 -0500

    ?

--- public/js/twerk.js
+++ public/js/twerk.js
@@ -4,62 +4,58 @@ var socket = io.connect('/')
 , iphone = navigator.userAgent.toLowerCase().indexOf("iphone");
 
 
-var sessionID= '';
+$(document).ready(function(){
+    var sessionID= '';
+    var socketID = null;
 
-var socketID = null;
-
-function joinSession() {
-    sessionID = prompt("enter room number:");
-    sessionID = sessionID.toString();
-    if (sessionID === "")
-	location.reload();
-    else {
-	socket.emit('join', sessionID);
-	console.log(socket.id);
-	socketID = socket.id;
+    function joinSession() {
+	sessionID = prompt("enter room number:");
+	sessionID = sessionID.toString();
+	if (sessionID === "")
+	    location.reload();
+	else {
+	    socket.emit('join', sessionID);
+	    console.log(socket.id);
+	    socketID = socket.id;
+	}
     }
-}
 
-var name = '';
-function getName() {
-  name = prompt("enter your name:");
-  $('#name').text(name);
-}
+    var name = '';
+    joinSession();
+    name = prompt("enter your name:"); 
+    $('#name').append(name);
 
-joinSession();
-getName();
+    socket.on('joined', function(data) {
+	socket.emit('named', name);
+	document.getElementById('audioLoop').play();
+    });
 
-socket.on('joined', function(data) {
-    socket.emit('named', name);
-    document.getElementById('audioLoop').play();
+
+    socket.on('end',function(data){
+    });
+
+    /*socket.on('player-update' ,function(data){
+      if (socketID == socket.id) {
+      //$('#name').text(data.name.toString());
+      }
+      });*/
+
+
+    // on shake 
+    window.addEventListener('shake', function(event) {
+	socket.emit('twerk', event.timeDifference);
+    }, false);
+
+    window.addEventListener('fall', function(event){
+	socket.emit('drop',event)
+    }, false); 
+
+    window.addEventListener('devicemotion', function(){
+	// socket.emit('device-motion', { 
+	// 	time: new Date().getTime(),
+	// 	x: event.accelerationIncludingGravity.x, 
+	// 	y: event.accelerationIncludingGravity.y, 
+	// 	z: event.accelerationIncludingGravity.z
+	// });
+    }, false);
 });
-
-
-socket.on('end',function(data){
-    console.log(data);
-});
-
-socket.on('player-update' ,function(data){
-    if (socketID == socket.id) {
-	//$('#name').text(data.name.toString());
-    }
-});
-
-
-// on shake 
-window.addEventListener('shake', function(event) {
-    socket.emit('twerk', event.timeDifference);
-}, false);
-
-window.addEventListener('fall', function(event){
-    socket.emit('drop',event)
-}, false); 
-
-window.addEventListener('devicemotion', function(){
-    // socket.emit('device-motion', { 
-    // 	time: new Date().getTime(),
-    // 	x: event.accelerationIncludingGravity.x, 
-    // 	y: event.accelerationIncludingGravity.y, 
-    // 	z: event.accelerationIncludingGravity.z
-    // });
-}, false);
